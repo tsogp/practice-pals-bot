de73e9ef39bb736040d9169944c66dabe181cccb
tsogp <specialforbtsu@gmail.com>
2022-01-16 22:23:48 +0300

    Add welcome message and main menu

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 import telebot
+import phrases_ru as phrases
 
 import bottoken
 
 bot = telebot.TeleBot(bottoken.TOKEN, parse_mode=None)
 
 
+def main_menu(chat_id):
+    """Main bot_menu"""
+    menu = telebot.types.InlineKeyboardMarkup()  # Create inline-keyboard
+    # Add all functions into menu
+    for i in range(len(phrases.main_menu_list)):
+        menu.add(telebot.types.InlineKeyboardButton(
+            text=phrases.main_menu_list[i],
+            callback_data=f'main_menu_{i + 1}'))
+
+    bot.send_message(chat_id, text=phrases.main_menu_title, reply_markup=menu)
+
+
+@bot.message_handler(commands=['menu'])
+def menu(message):
+    """Show main menu"""
+    main_menu(message.chat.id)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    """Inline-keyboards button's click handler"""
+    bot.answer_callback_query(callback_query_id=call.id, text='')
+    if call.data == 'main_menu_1':
+        bot.send_message(call.message.chat.id,
+                         text="OK")
+
+
 @bot.message_handler(commands=['start'])
-def welcome(message):
-    bot.reply_to(message, "Добрый день!")
+def start(message):
+    bot.send_message(message.chat.id,
+                     text=phrases.welcome_message,
+                     reply_markup=telebot.types.ReplyKeyboardRemove())
+    main_menu(message.chat.id)
 
 
-bot.infinity_polling()
+if __name__ == '__main__':  # Run
+    bot.polling(none_stop=True)
---
+++ phrases_ru.py
@@ -0,0 +1,7 @@
+# File with phrases for the interface in Russian
+
+welcome_message = "Привет! Я Practice Pals Bot. Помогу тебе найти друзей по интересам в IT-сфере"
+
+main_menu_title = "Главное меню бота:"
+
+main_menu_list = ["Найти единомышленников"]
