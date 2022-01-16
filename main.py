9eb07de7e0b3c8ddf0fcd407d1e9192c3eb15500
tsogp <specialforbtsu@gmail.com>
2022-01-16 21:34:06 +0300

    Hide token

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import telebot
 
-bot = telebot.TeleBot("5005216647:AAHeTj3pMtbKUz8E_N63YYRNEMfs9X0FY5c", parse_mode=None)
+import bottoken
+
+bot = telebot.TeleBot(bottoken.TOKEN, parse_mode=None)
+
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
     bot.reply_to(message, "Добрый день!")
+
 
 bot.infinity_polling()
