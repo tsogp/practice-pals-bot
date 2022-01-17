8971d9a7c26081cbe4426ffc84a72a52e2a01a29
tsogp <specialforbtsu@gmail.com>
2022-01-17 23:49:40 +0300

    Change keyboard's view

--- keyboards.py
+++ keyboards.py
@@ -6,17 +6,20 @@ class Keyboards:
 
     @staticmethod
     def __create_keyboard_do_not_specify():
-        keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2,
-                                                     resize_keyboard=True)
+        keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
         keyboard.add(telebot.types.KeyboardButton(text=phrases.do_not_specify))
         return keyboard
 
     @staticmethod
     def __create_keyboard_with_multiple_choice(items_list: list):
-        keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2,
-                                                     resize_keyboard=True)
-        for language in items_list:
-            keyboard.add(telebot.types.KeyboardButton(text=language))
+        keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+
+        for i in range(0, len(items_list), 2):
+            keyboard.add(telebot.types.KeyboardButton(text=items_list[i]),
+                         telebot.types.KeyboardButton(text=items_list[i + 1]))
+        if len(items_list) % 2 != 0:
+            keyboard.add(telebot.types.KeyboardButton(text=items_list[-1]))
+
         keyboard.add(telebot.types.KeyboardButton(text=phrases.finish_typing))
         keyboard.add(telebot.types.KeyboardButton(text=phrases.do_not_specify))
         return keyboard
