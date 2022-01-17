0778236eaaca96a6eb8abd50f042e95513c6781a
tsogp <specialforbtsu@gmail.com>
2022-01-17 23:59:15 +0300

    Refactoring processing text msg

--- bot.py
+++ bot.py
@@ -79,83 +79,101 @@ class Bot:
 
         if users_menu_id == constants.MenuIds.REGISTRATION_MENU:
             if users_registration_item_id == constants.RegistationItemsIds.FIRST_NAME:
-                if users_message == phrases.do_not_specify:
-                    Bot.__database.write_empty_users_registration_item(telegram_id=message.chat.id,
-                                                                       item=constants.RegistationItemsIds.FIRST_NAME)
-                else:
-                    Bot.__database.write_users_registration_item(telegram_id=message.chat.id,
-                                                                 item=constants.RegistationItemsIds.FIRST_NAME,
-                                                                 value=users_message)
-
-                Bot.__database.switch_user_to_next_registration_item(telegram_id=message.chat.id)
-                Bot.__bot.send_message(message.chat.id, text=phrases.enter_your_last_name)
-
+                Bot.__processing_registration_item_first_name(users_message, chat_id=message.chat.id)
             elif users_registration_item_id == constants.RegistationItemsIds.LAST_NAME:
-                if users_message == phrases.do_not_specify:
-                    Bot.__database.write_empty_users_registration_item(telegram_id=message.chat.id,
-                                                                       item=constants.RegistationItemsIds.LAST_NAME)
-                else:
-                    Bot.__database.write_users_registration_item(telegram_id=message.chat.id,
-                                                                 item=constants.RegistationItemsIds.LAST_NAME,
-                                                                 value=users_message)
-
-                Bot.__database.switch_user_to_next_registration_item(telegram_id=message.chat.id)
-                Bot.__bot.send_message(message.chat.id, text=phrases.enter_your_age)
-
+                Bot.__processing_registration_item_last_name(users_message, chat_id=message.chat.id)
             elif users_registration_item_id == constants.RegistationItemsIds.AGE:
-                if users_message == phrases.do_not_specify:
-                    Bot.__database.write_empty_users_registration_item(telegram_id=message.chat.id,
-                                                                       item=constants.RegistationItemsIds.AGE)
-                else:
-                    Bot.__database.write_users_registration_item(telegram_id=message.chat.id,
-                                                                 item=constants.RegistationItemsIds.AGE,
-                                                                 value=users_message)
-
-                Bot.__database.switch_user_to_next_registration_item(telegram_id=message.chat.id)
-                Bot.__bot.send_message(message.chat.id, text=phrases.enter_your_spoken_languages,
-                                       reply_markup=Keyboards.keyboard_spoken_languages)
-
+                Bot.__processing_registration_item_age(users_message, chat_id=message.chat.id)
             elif users_registration_item_id == constants.RegistationItemsIds.SPOKEN_LANGUAGES:
-                if users_message == phrases.do_not_specify:
-                    Bot.__database.write_empty_users_registration_item(telegram_id=message.chat.id,
-                                                                       item=constants.RegistationItemsIds.SPOKEN_LANGUAGES)
-                elif users_message != phrases.finish_typing:
-                    Bot.__database.write_users_registration_item(telegram_id=message.chat.id,
-                                                                 item=constants.RegistationItemsIds.SPOKEN_LANGUAGES,
-                                                                 value=users_message)
-
-                if users_message in (phrases.do_not_specify, phrases.finish_typing):
-                    Bot.__database.switch_user_to_next_registration_item(telegram_id=message.chat.id)
-                    Bot.__bot.send_message(message.chat.id, text=phrases.enter_your_programming_languages,
-                                           reply_markup=Keyboards.keyboard_programming_languages)
-
+                Bot.__processing_registration_item_spoken_language(users_message, chat_id=message.chat.id)
             elif users_registration_item_id == constants.RegistationItemsIds.PROGRAMMING_LANGUAGES:
-                if users_message == phrases.do_not_specify:
-                    Bot.__database.write_empty_users_registration_item(telegram_id=message.chat.id,
-                                                                       item=constants.RegistationItemsIds.PROGRAMMING_LANGUAGES)
-                elif users_message != phrases.finish_typing:
-                    Bot.__database.write_users_registration_item(telegram_id=message.chat.id,
-                                                                 item=constants.RegistationItemsIds.PROGRAMMING_LANGUAGES,
-                                                                 value=users_message)
-
-                if users_message in (phrases.do_not_specify, phrases.finish_typing):
-                    Bot.__database.switch_user_to_next_registration_item(telegram_id=message.chat.id)
-                    Bot.__bot.send_message(message.chat.id, text=phrases.enter_your_interests,
-                                           reply_markup=Keyboards.keyboard_interests)
-
+                Bot.__processing_registration_item_programming_language(users_message, chat_id=message.chat.id)
             elif users_registration_item_id == constants.RegistationItemsIds.INTERESTS:
-                if users_message == phrases.do_not_specify:
-                    Bot.__database.write_empty_users_registration_item(telegram_id=message.chat.id,
-                                                                       item=constants.RegistationItemsIds.INTERESTS)
-                elif users_message != phrases.finish_typing:
-                    Bot.__database.write_users_registration_item(telegram_id=message.chat.id,
-                                                                 item=constants.RegistationItemsIds.INTERESTS,
-                                                                 value=users_message)
-
-                if users_message in (phrases.do_not_specify, phrases.finish_typing):
-                    Bot.__database.switch_user_to_next_registration_item(telegram_id=message.chat.id)
-                    Bot.__bot.send_message(message.chat.id, text=phrases.finish_registration,
-                                           reply_markup=telebot.types.ReplyKeyboardRemove())
-
+                Bot.__processing_registration_item_interests(users_message, chat_id=message.chat.id)
         else:
             Bot.__bot.send_message(message.chat.id, text="Bla-bla-bla")
+
+    @staticmethod
+    def __processing_registration_item_first_name(users_message, chat_id):
+        if users_message == phrases.do_not_specify:
+            Bot.__database.write_empty_users_registration_item(telegram_id=chat_id,
+                                                               item=constants.RegistationItemsIds.FIRST_NAME)
+        else:
+            Bot.__database.write_users_registration_item(telegram_id=chat_id,
+                                                         item=constants.RegistationItemsIds.FIRST_NAME,
+                                                         value=users_message)
+
+        Bot.__database.switch_user_to_next_registration_item(telegram_id=chat_id)
+        Bot.__bot.send_message(chat_id, text=phrases.enter_your_last_name)
+
+    @staticmethod
+    def __processing_registration_item_last_name(users_message, chat_id):
+        if users_message == phrases.do_not_specify:
+            Bot.__database.write_empty_users_registration_item(telegram_id=chat_id,
+                                                               item=constants.RegistationItemsIds.LAST_NAME)
+        else:
+            Bot.__database.write_users_registration_item(telegram_id=chat_id,
+                                                         item=constants.RegistationItemsIds.LAST_NAME,
+                                                         value=users_message)
+
+        Bot.__database.switch_user_to_next_registration_item(telegram_id=chat_id)
+        Bot.__bot.send_message(chat_id, text=phrases.enter_your_age)
+
+    @staticmethod
+    def __processing_registration_item_age(users_message, chat_id):
+        if users_message == phrases.do_not_specify:
+            Bot.__database.write_empty_users_registration_item(telegram_id=chat_id,
+                                                               item=constants.RegistationItemsIds.AGE)
+        else:
+            Bot.__database.write_users_registration_item(telegram_id=chat_id,
+                                                         item=constants.RegistationItemsIds.AGE,
+                                                         value=users_message)
+
+        Bot.__database.switch_user_to_next_registration_item(telegram_id=chat_id)
+        Bot.__bot.send_message(chat_id, text=phrases.enter_your_spoken_languages,
+                               reply_markup=Keyboards.keyboard_spoken_languages)
+
+    @staticmethod
+    def __processing_registration_item_spoken_language(users_message, chat_id):
+        if users_message == phrases.do_not_specify:
+            Bot.__database.write_empty_users_registration_item(telegram_id=chat_id,
+                                                               item=constants.RegistationItemsIds.SPOKEN_LANGUAGES)
+        elif users_message != phrases.finish_typing:
+            Bot.__database.write_users_registration_item(telegram_id=chat_id,
+                                                         item=constants.RegistationItemsIds.SPOKEN_LANGUAGES,
+                                                         value=users_message)
+
+        if users_message in (phrases.do_not_specify, phrases.finish_typing):
+            Bot.__database.switch_user_to_next_registration_item(telegram_id=chat_id)
+            Bot.__bot.send_message(chat_id, text=phrases.enter_your_programming_languages,
+                                   reply_markup=Keyboards.keyboard_programming_languages)
+
+    @staticmethod
+    def __processing_registration_item_programming_language(users_message, chat_id):
+        if users_message == phrases.do_not_specify:
+            Bot.__database.write_empty_users_registration_item(telegram_id=chat_id,
+                                                               item=constants.RegistationItemsIds.PROGRAMMING_LANGUAGES)
+        elif users_message != phrases.finish_typing:
+            Bot.__database.write_users_registration_item(telegram_id=chat_id,
+                                                         item=constants.RegistationItemsIds.PROGRAMMING_LANGUAGES,
+                                                         value=users_message)
+
+        if users_message in (phrases.do_not_specify, phrases.finish_typing):
+            Bot.__database.switch_user_to_next_registration_item(telegram_id=chat_id)
+            Bot.__bot.send_message(chat_id, text=phrases.enter_your_interests,
+                                   reply_markup=Keyboards.keyboard_interests)
+
+    @staticmethod
+    def __processing_registration_item_interests(users_message, chat_id):
+        if users_message == phrases.do_not_specify:
+            Bot.__database.write_empty_users_registration_item(telegram_id=chat_id,
+                                                               item=constants.RegistationItemsIds.INTERESTS)
+        elif users_message != phrases.finish_typing:
+            Bot.__database.write_users_registration_item(telegram_id=chat_id,
+                                                         item=constants.RegistationItemsIds.INTERESTS,
+                                                         value=users_message)
+
+        if users_message in (phrases.do_not_specify, phrases.finish_typing):
+            Bot.__database.switch_user_to_next_registration_item(telegram_id=chat_id)
+            Bot.__bot.send_message(chat_id, text=phrases.finish_registration,
+                                   reply_markup=telebot.types.ReplyKeyboardRemove())
